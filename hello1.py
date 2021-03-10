2235fba55d361ee4556f8125d2a38abf8dc35d2c
padma112345 <80413019+padma112345@users.noreply.github.com>
2021-03-10 21:53:54 +0530

    Add files via upload

---
+++ hello1.py
@@ -0,0 +1,25 @@
+#from flask import Flask
+#app = Flask(__name__)
+
+#@app.route('/')
+#def hello_world():
+#    return 'Hello World!'
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
